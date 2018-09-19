17fec00c9d117ea68271bba6c4b8f19eeba3ea19
Sven Bröckling <sven@nimbostratus.de>
2018-09-19 19:23:18 +0200

    Changed fa icon class to be the complete fa class

--- rulebook/models.py
+++ rulebook/models.py
@@ -12,7 +12,7 @@ class Chapter(models.Model, metaclass=TransMeta):
 
     number = models.IntegerField(_('number'))
     name = models.CharField(_('name'), max_length=120)
-    fa_icon_class = models.CharField(_('FA Icon Class'), max_length=30, default='book')
+    fa_icon_class = models.CharField(_('FA Icon Class'), max_length=30, default='fa fa-book')
     text = SimpleMDEField(_('text'), blank=True, null=True)
     extension = models.ForeignKey('rules.Extension', models.CASCADE)
 
