265a43393507cd1f04c598e604787e2a256a1030
Sven Bröckling <sven@nimbostratus.de>
2018-09-30 11:43:44 +0200

    Colorful values

--- characters/templatetags/characters_extras.py
+++ characters/templatetags/characters_extras.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.utils.safestring import mark_safe
 
 register = Library()
 
@@ -9,3 +10,30 @@ def character_attribute_widget(name, attribute):
         'name': name,
         'attribute': attribute,
     }
+
+
+@register.simple_tag
+def color_value_span(value, max_value, invert=False):
+    value = int(value)
+    max_value = int(max_value)
+
+    if invert:
+        value = max_value - value
+
+    try:
+        p = (value * 100) / max_value
+    except ZeroDivisionError:  # max_value == 0
+        p = 0
+
+    p = 100 if p > 100 else p
+
+    if p > 80:
+        color = '#47C040'
+    elif p > 60:
+        color = '#C0BC40'
+    elif p > 20:
+        color = '#ECA935'
+    else:
+        color = '#EC4D35'
+    return mark_safe('<span title="max. %s" style="font-weight: bold; color:%s;">%s</span>' % (max_value, color, value))
+
