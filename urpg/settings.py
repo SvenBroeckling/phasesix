57d92731c411fbd3a0a6ef19d89e0bc726fe1100
Sven Broeckling <sven@nimbostratus.de>
2019-09-30 16:20:27 +0200

    Compress settings

--- urpg/settings.py
+++ urpg/settings.py
@@ -113,6 +113,8 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_ENABLED = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
 
