56dbfe7a6e82c6ce30a7f7e5cc95822f6eeb7f54
Sven Bröckling <sven@nimbostratus.de>
2019-10-02 12:35:49 +0200

    Delete Anonymous Character Command

--- campaigns/models.py
+++ campaigns/models.py
@@ -5,10 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 class Campaign(models.Model):
     name = models.CharField(_('name'), max_length=80)
     image = models.ImageField(
-        _('image'), upload_to='character_images', blank=True, null=True
+        _('image'), upload_to='campaign_images', blank=True, null=True
     )
     backdrop_image = models.ImageField(
-        _('backdrop image'), upload_to='character_backdrop_images', blank=True, null=True
+        _('backdrop image'), upload_to='campaign_backdrop_images', blank=True, null=True
     )
     abstract = models.TextField(_('abstract'), blank=True, null=True)
 
@@ -36,6 +36,6 @@ class Handout(models.Model):
     scene = models.ForeignKey(Scene, on_delete=models.CASCADE)
     name = models.CharField(_('name'), max_length=80)
     image = models.ImageField(
-        _('image'), upload_to='character_images', blank=True, null=True
+        _('image'), upload_to='campaign_handouts', blank=True, null=True
     )
 
