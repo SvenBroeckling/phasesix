cc22bf0b4d9e3e77adce6d8f1b412ffb380abb95
Sven Bröckling <sven@broeckling.de>
2021-05-30 02:20:41 +0200

    Django3

--- urpg/settings.py
+++ urpg/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from django.urls import reverse_lazy
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_URL = 'http://phasesix.org'
