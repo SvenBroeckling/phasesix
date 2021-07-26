8fc489bfd6f611337aed137d7f5f2052b9248254
Sven Bröckling <sven@broeckling.de>
2021-07-26 14:36:30 +0200

    Spell Admin

--- magic/admin.py
+++ magic/admin.py
@@ -6,6 +6,14 @@ from magic.models import BaseSpell, SpellType, SpellVariant, SpellShape, SpellTe
 class BaseSpellAdmin(admin.ModelAdmin):
     list_display = ('name_de', 'name_en', 'cost', 'type', 'variant', 'power', 'range', 'actions')
     list_filter = ('cost', 'type', 'variant', 'power', 'range', 'actions')
+    fields = (
+        ('name_de', 'name_en'),
+        ('cost', 'power', 'range', 'actions'),
+        ('variant', 'type', 'shape'),
+        'is_ritual',
+        ('rules_de', 'rules_en'),
+        ('quote', 'quote_author'),
+    )
 
 
 class SpellTemplateModifierInline(admin.TabularInline):
@@ -14,6 +22,12 @@ class SpellTemplateModifierInline(admin.TabularInline):
 
 class SpellTemplateAdmin(admin.ModelAdmin):
     inlines = [SpellTemplateModifierInline]
+    fields = (
+        ('name_de', 'name_en'),
+        'cost',
+        ('rules_de', 'rules_en'),
+        ('quote', 'quote_author'),
+    )
 
 
 admin.site.register(BaseSpell, BaseSpellAdmin)
