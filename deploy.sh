adc9fdca2ce9e6484966f646b5cc7ab6f4d878a3
Sven Bröckling <sven@broeckling.de>
2021-12-17 21:12:04 +0100

    removed fabfile

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ssh sven@nimbostratus.de "cd www/urpg/www ; git pull"
+ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/pip install -U pip"
+ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/pip install -r requirements.txt"
+
+ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/python manage.py migrate"
+ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/python manage.py collectstatic --no-input"
+
+ssh sven@nimbostratus.de "sudo systemctl restart urpg"
