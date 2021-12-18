ccea7c857b89edb45304643244344bf4db7d1769
Sven Bröckling <sven@broeckling.de>
2021-12-18 15:24:36 +0100

    Updated tmux

--- tmux.sh
+++ tmux.sh
@@ -8,7 +8,7 @@ tmux new-session -s $SESSION_NAME -n sql -d
 tmux new-window -n celery -t $SESSION_NAME
 #tmux send-keys -t $SESSION_NAME:1 'workon magily; celery -A magily worker --events --beat --loglevel DEBUG --concurrency=1 -Q default,books' C-m
 tmux new-window -n shell -t $SESSION_NAME
-tmux send-keys -t $SESSION_NAME:2 'workon urpg; fab update; ./manage.py runserver 0.0.0.0:8000' C-m
+tmux send-keys -t $SESSION_NAME:2 'workon urpg; ./update.sh; ./manage.py runserver 0.0.0.0:8000' C-m
 tmux split-window -h -t $SESSION_NAME:2
 tmux send-keys -t $SESSION_NAME:2 'workon urpg' C-m
 
