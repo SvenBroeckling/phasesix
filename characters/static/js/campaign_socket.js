beba7c0361fe4628a26626b1193a77abb2d94d5d
Sven Bröckling <sven@broeckling.de>
2022-01-13 21:30:28 +0100

    Dice socket reconnect

--- characters/static/js/campaign_socket.js
+++ characters/static/js/campaign_socket.js
@@ -1,26 +1,37 @@
 $(function(){
-    let socket = new WebSocket($('#room-url').text())
+    let socket = new ReconnectingWebSocket($('#room-url').text(), null, {reconnectInterval: 3000})
+    let indicator = $('#dice-ws-connected')
 
+    socket.onopen = (e) => {
+        indicator.removeClass('text-warning')
+            .addClass('text-primary')
+            .attr('title', indicator.data('message-connected'))
+    }
     socket.onerror = (e) => {
-        console.error(`Connection error: ${e}`)
+        indicator.removeClass('text-primary')
+            .addClass('text-warning')
+            .attr('title', indicator.data('message-disconnected'))
+    }
+
+    socket.onclose = (e) => {
+        indicator.removeClass('text-primary')
+            .addClass('text-warning')
+            .attr('title', indicator.data('message-disconnected'))
     }
 
     socket.onmessage = (e) => {
         const data = JSON.parse(e.data)
-        const displayResults = $('#display-ws-dice-results').prop('checked')
         const audioElement = document.getElementById('room-audio')
 
-        if(displayResults){
-            Toast.setPlacement(TOAST_PLACEMENT.BOTTOM_LEFT)
-            Toast.setMaxCount(7)
-            Toast.create({
-                title: data.message.character,
-                message: `${data.message.header} <small class="text-muted">${data.message.description}</small><br>${data.message.result_html}`,
-                status: TOAST_STATUS.SUCCESS,
-                timeout: 10000,
-            })
-            audioElement.play()
-        }
+        Toast.setPlacement(TOAST_PLACEMENT.BOTTOM_LEFT)
+        Toast.setMaxCount(7)
+        Toast.create({
+            title: data.message.character,
+            message: `${data.message.header} <small class="text-muted">${data.message.description}</small><br>${data.message.result_html}`,
+            status: TOAST_STATUS.SUCCESS,
+            timeout: 10000,
+        })
+        audioElement.play()
     }
 
     $('body').on('click', '.dice-roll', function(e) {
