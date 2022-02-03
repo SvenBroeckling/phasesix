40d7961fbc3e1979993d4680b092c3e6df99914d
Sven Bröckling <sven@broeckling.de>
2022-02-03 12:15:38 +0100

    Re-enabled asgi http

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/pip install 
 ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/python manage.py migrate"
 ssh sven@nimbostratus.de "cd www/urpg/www ; ~/.virtualenvs/urpg/bin/python manage.py collectstatic --no-input"
 
-ssh sven@nimbostratus.de "sudo systemctl restart urpg"
+ssh sven@nimbostratus.de "sudo systemctl restart urpg-ws"
+ssh sven@nimbostratus.de "sudo systemctl restart urpg-http"
--- urpg/asgi.py
+++ urpg/asgi.py
@@ -2,12 +2,14 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 import campaigns.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urpg.settings')
 
 application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             campaigns.routing.websocket_urlpatterns
