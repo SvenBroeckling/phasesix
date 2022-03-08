d488c9337a76d1f95605f014ee2cc43c6137ca64
Sven Bröckling <sven@broeckling.de>
2022-03-08 19:20:40 +0100

    Roll Admin

--- campaigns/admin.py
+++ campaigns/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from campaigns.models import Campaign
+from campaigns.models import Campaign, Roll
+
+
+class RollAdmin(admin.ModelAdmin):
+    list_display = ('campaign', 'character', 'header', 'roll_string', 'results_csv')
+    list_filter = ('campaign', 'character')
+
 
 admin.site.register(Campaign)
+admin.site.register(Roll, RollAdmin)
