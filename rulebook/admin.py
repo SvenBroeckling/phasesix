2872facd71e4b8abe0cb6a037dd029a0f26209bb
Sven Bröckling <sven@broeckling.de>
2022-09-01 10:52:13 +0200

    Chapter Admin

--- rulebook/admin.py
+++ rulebook/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from rulebook.models import Book, Chapter
 
+
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = 'name_de', 'name_en', 'number', 'fa_icon_class'
+
+
 admin.site.register(Book)
-admin.site.register(Chapter)
+admin.site.register(Chapter, ChapterAdmin)
 
 
