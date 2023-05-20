fb1db2b3bc414c8a9d571076463235470dbf00e3
Sven Bröckling <sven@broeckling.de>
2023-05-20 20:19:02 +0200

    Wiki Layout, requirements update, charactertemplate inline

--- characters/admin.py
+++ characters/admin.py
@@ -3,7 +3,11 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from characters.models import Character
+from characters.models import Character, CharacterTemplate
+
+
+class CharacterTemplateInline(admin.TabularInline):
+    model = CharacterTemplate
 
 
 class CharacterAdmin(admin.ModelAdmin):
@@ -11,6 +15,7 @@ class CharacterAdmin(admin.ModelAdmin):
     list_editable = 'may_appear_on_start_page',
     list_filter = 'created_by', 'campaign', 'extensions'
     search_fields = 'name',
+    inlines = [CharacterTemplateInline]
 
 
 admin.site.register(Character, CharacterAdmin)
