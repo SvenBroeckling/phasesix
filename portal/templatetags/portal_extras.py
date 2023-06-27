31730ac6864f2c50e0ce218fee58dcfa29c0e76b
Sven Bröckling <sven@broeckling.de>
2023-06-27 23:50:45 +0200

    Adjusted main_nav for sidebar nav

---
+++ portal/templatetags/portal_extras.py
@@ -0,0 +1,48 @@
+from django.template import Library
+from django.urls import reverse
+
+register = Library()
+
+
+@register.simple_tag(takes_context=True)
+def active_if_url_name_matches(context, url_name, **kwargs):
+    resolver_match = context['request'].resolver_match
+    if url_name == 'worlds:detail':
+        print(resolver_match)
+
+    try:
+        app_name, name = url_name.split(":")
+    except ValueError:
+        if resolver_match.url_name == url_name:
+            if kwargs is not None and resolver_match.kwargs != kwargs:
+                return ''
+            return 'active'
+    else:
+        if app_name in resolver_match.app_name and resolver_match.url_name == name:
+            if kwargs is not None and resolver_match.kwargs != kwargs:
+                return ''
+            return 'active'
+    return ''
+
+
+@register.simple_tag
+def create_character_url(world):
+    if not world or not world.extension:
+        return reverse("characters:create_character")
+
+    if not world.extension.fixed_epoch:
+        return reverse(
+            "characters:create_character_epoch",
+            kwargs={"world_pk": world.extension.id}
+        )
+
+    if not world.extension.fixed_extensions.exists():
+        return reverse(
+            "characters:create_character_extensions",
+            kwargs={"world_pk": world.extension.id, "epoch_pk": world.extension.fixed_epoch.id})
+
+    return reverse(
+        "characters:create_character_data",
+        kwargs={"world_pk": world.extension.id, "epoch_pk": world.extension.fixed_epoch.id})
+
+
