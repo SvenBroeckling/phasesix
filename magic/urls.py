3a3a30133e6bb6b4d4d10fceaa9764ef25abe56c
Sven Bröckling <sven@broeckling.de>
2023-09-10 06:47:26 +0200

    Split the spell list into origins; fixed the lightbox close button not visible

--- magic/urls.py
+++ magic/urls.py
@@ -5,6 +5,7 @@ from magic import views
 app_name = 'magic'
 
 urlpatterns = [
-    path('spell/list/', views.BaseSpellListView.as_view(), name='basespell_list'),
+    path('spell/origins/', views.SpellOriginView.as_view(), name='spell_origin_list'),
+    path('spell/list/<int:origin_pk>', views.BaseSpellListView.as_view(), name='basespell_list'),
     path('spell/detail/<int:pk>', views.BaseSpellDetailView.as_view(), name='basespell_detail'),
 ]
--- magic/views.py
+++ magic/views.py
@@ -1,11 +1,22 @@
 from django.views.generic import ListView, DetailView
 
-from magic.models import BaseSpell
+from magic.models import BaseSpell, SpellOrigin
+
+
+class SpellOriginView(ListView):
+    model = SpellOrigin
 
 
 class BaseSpellListView(ListView):
     def get_queryset(self):
-        return BaseSpell.objects.without_homebrew()
+        return BaseSpell.objects.without_homebrew().filter(
+            origin__id=self.kwargs["origin_pk"]
+        )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["origin"] = SpellOrigin.objects.get(id=self.kwargs["origin_pk"])
+        return context
 
 
 class BaseSpellDetailView(DetailView):
