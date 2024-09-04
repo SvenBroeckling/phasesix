d02fa7a5527ddf88eb9257b169f849b4bb331888
Sven <sven@nimbostratus.de>
2024-09-04 23:30:35 +0200

    Homebrew Item Creation: Removed staff only decorator

--- homebrew/urls.py
+++ homebrew/urls.py
@@ -1,46 +1,35 @@
-from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from homebrew import views
 
-app_name = 'homebrew'
+app_name = "homebrew"
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-
+    path("", views.IndexView.as_view(), name="index"),
+    path("xhr_create/item", views.XhrCreateItemView.as_view(), name="xhr_create_item"),
+    path("create/item", views.CreateItemView.as_view(), name="create_item"),
     path(
-        'xhr_create/item',
-        staff_member_required(views.XhrCreateItemView.as_view()),
-        name='xhr_create_item'),
-    path(
-        'create/item',
-        views.CreateItemView.as_view(),
-        name='create_item'),
-
-    path(
-        'xhr_create/riot_gear',
+        "xhr_create/riot_gear",
         views.XhrCreateRiotGearView.as_view(),
-        name='xhr_create_riot_gear'),
+        name="xhr_create_riot_gear",
+    ),
     path(
-        'create/riot_gear',
-        views.CreateRiotGearView.as_view(),
-        name='create_riot_gear'),
-
+        "create/riot_gear", views.CreateRiotGearView.as_view(), name="create_riot_gear"
+    ),
     path(
-        'xhr_create/weapon',
+        "xhr_create/weapon",
         views.XhrCreateWeaponView.as_view(),
-        name='xhr_create_weapon'),
+        name="xhr_create_weapon",
+    ),
+    path("create/weapon", views.CreateWeaponView.as_view(), name="create_weapon"),
     path(
-        'create/weapon',
-        views.CreateWeaponView.as_view(),
-        name='create_weapon'),
-
-    path(
-        'xhr_create/basespell',
+        "xhr_create/basespell",
         views.XhrCreateBaseSpellView.as_view(),
-        name='xhr_create_base_spell'),
+        name="xhr_create_base_spell",
+    ),
     path(
-        'create/basespell',
+        "create/basespell",
         views.CreateBaseSpellView.as_view(),
-        name='create_base_spell'),
+        name="create_base_spell",
+    ),
 ]
