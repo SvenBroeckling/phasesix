15fe16fadcfcefb882e0c073f5ef8ebb04c8f809
Sven <sven@nimbostratus.de>
2024-11-11 22:39:15 +0100

    Unfold User/Group Admin

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User, Group
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+
+class UserAdmin(ModelAdmin, BaseUserAdmin):
+    pass
+
+
+class GroupAdmin(ModelAdmin, BaseGroupAdmin):
+    pass
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.register(Group, GroupAdmin)
