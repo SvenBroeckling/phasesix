9ae694d5014e3bd8d4aae57ad9144bff6a8e5e5e
Sven <sven@nimbostratus.de>
2024-12-03 17:52:27 +0100

    Wrapup 2024

--- portal/views.py
+++ portal/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q, Count
 from django.db.models.functions import Trunc, Length
 from django.shortcuts import redirect
+from django.templatetags.static import static
 from django.views import View
 from django.views.generic import TemplateView, DetailView
 
@@ -162,13 +163,7 @@ class YearlyWrapUpView(TemplateView):
             .order_by("-total")[:3]
         ):
             character = Character.objects.get(id=mp["character"])
-            days = (
-                qs.filter(character=character)
-                .annotate(created_at_day=Trunc("created_at", "day"))
-                .values("created_at_day")
-                .annotate(total=Count("created_at_day"))
-                .count()
-            )
+            days = self._get_days(qs, character)
             res.append((character, mp["total"], days))
         return res
 
@@ -187,28 +182,68 @@ class YearlyWrapUpView(TemplateView):
         except IndexError:
             return None
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    def get_platform_stats(self):
+        qs = Roll.objects.filter(created_at__year=self.kwargs["year"])
+        return {
+            "platform_crit_count": self.get_highest_roll(qs, "crit_count"),
+            "platform_exploded_dice_count": self.get_highest_roll(
+                qs, "exploded_dice_count"
+            ),
+            "platform_successes_count": self.get_highest_roll(qs, "successes_count"),
+            "platform_fails_count": self.get_highest_roll(qs, "fails_count"),
+            "platform_highest_single_roll": self.get_highest_roll(
+                qs, "highest_single_roll"
+            ),
+            "platform_total_sum": self.get_highest_roll(qs, "total_sum"),
+            "platform_roll_count": qs.count(),
+            "platform_days": self._get_days(qs),
+            "platform_characters_created": Character.objects.filter(
+                created_at__year=self.kwargs["year"]
+            ).count(),
+            "platform_campaigns_created": Campaign.objects.filter(
+                created_at__year=self.kwargs["year"]
+            ).count(),
+        }
+
+    def get_user_stats(self):
         qs = Roll.objects.filter(
             character__created_by=User.objects.get(id=self.kwargs["pk"]),
             created_at__year=self.kwargs["year"],
         )
-        context.update(
-            {
-                "year": self.kwargs["year"],
-                "qs": qs,
-                "most_played": self.get_most_played(qs),
-                "played_campaigns": self.get_played_campaigns(qs),
-                "roll_crit_count": self.get_highest_roll(qs, "crit_count"),
-                "roll_exploded_dice_count": self.get_highest_roll(
-                    qs, "exploded_dice_count"
-                ),
-                "roll_successes_count": self.get_highest_roll(qs, "successes_count"),
-                "roll_fails_count": self.get_highest_roll(qs, "fails_count"),
-                "roll_highest_single_roll": self.get_highest_roll(
-                    qs, "highest_single_roll"
-                ),
-                "roll_total_sum": self.get_highest_roll(qs, "total_sum"),
-            }
-        )
+        return {
+            "wrapup_user": User.objects.get(id=self.kwargs["pk"]),
+            "year": self.kwargs["year"],
+            "qs": qs,
+            "most_played": self.get_most_played(qs),
+            "played_campaigns": self.get_played_campaigns(qs),
+            "roll_crit_count": self.get_highest_roll(qs, "crit_count"),
+            "roll_exploded_dice_count": self.get_highest_roll(
+                qs, "exploded_dice_count"
+            ),
+            "roll_successes_count": self.get_highest_roll(qs, "successes_count"),
+            "roll_fails_count": self.get_highest_roll(qs, "fails_count"),
+            "roll_highest_single_roll": self.get_highest_roll(
+                qs, "highest_single_roll"
+            ),
+            "roll_total_sum": self.get_highest_roll(qs, "total_sum"),
+        }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(self.get_user_stats())
+        context.update(self.get_platform_stats())
+        context["header_image"] = static("img/wrapup_2024.png")
+        if self.kwargs["year"] == 2023:
+            context["header_image"] = static("img/wrapup_2023.png")
         return context
+
+    def _get_days(self, qs, character=None):
+        if character:
+            qs = qs.filter(character=character)
+
+        return (
+            qs.annotate(created_at_day=Trunc("created_at", "day"))
+            .values("created_at_day")
+            .annotate(total=Count("created_at_day"))
+            .count()
+        )
