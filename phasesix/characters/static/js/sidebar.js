66f5bc84ea18d632b503a0be049f881798f6678f
Sven <sven@nimbostratus.de>
2025-06-14 18:24:17 +0200

    Use loader also for sidebar

--- phasesix/characters/static/js/sidebar.js
+++ phasesix/characters/static/js/sidebar.js
@@ -1,21 +1,22 @@
 $(function () {
-    let body = $('body')
-    body.on('click', '.sidebar-trigger', function (e) {
-        let sidebar = $('#sidebar-right');
-        $(sidebar).css('width', '330px');
-        sidebar.find('h4:first').text($(this).data('sidebar-title'))
-        if ($(this).hasClass('sidebar-spinner')) {
-            sidebar.find('.sidebar-content').html('<div class="w-100 pt-5 d-flex justify-content-center align-content-center"><i class="fas fa-spinner fa-3x fa-spin"></i></div>')
+    let body = $("body");
+    body.on("click", ".sidebar-trigger", function (e) {
+        let sidebar = $("#sidebar-right");
+        $(sidebar).css("width", "330px");
+        sidebar.find("h4:first").text($(this).data("sidebar-title"));
+        if ($(this).hasClass("sidebar-spinner")) {
+            sidebar.find(".sidebar-content").html('<div class="loader"></div>');
         }
-        sidebar.find('.sidebar-content')
-            .data('sidebar-url', $(this).data('sidebar-url'))
-            .load($(this).data('sidebar-url'))
+        sidebar
+            .find(".sidebar-content")
+            .data("sidebar-url", $(this).data("sidebar-url"))
+            .load($(this).data("sidebar-url"));
         e.preventDefault();
         return false;
-    })
+    });
 
-    body.on('click', '.sidebar-close', function () {
-        $('#sidebar-right').css('width', '');
+    body.on("click", ".sidebar-close", function () {
+        $("#sidebar-right").css("width", "");
         return false;
     });
-})
+});
