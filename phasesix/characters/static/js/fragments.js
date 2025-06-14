d13df607ba4acd1b98b87f928cb1ca9fb0b915bb
Sven <sven@nimbostratus.de>
2025-06-14 17:15:56 +0200

    Simplified csrf token javascript

--- phasesix/characters/static/js/fragments.js
+++ phasesix/characters/static/js/fragments.js
@@ -2,28 +2,36 @@ let timeout = null;
 
 function refresh_fragments() {
     function refresh() {
-        $('.fragment').each(function (index) {
-            let active_href = $(".character-main-nav").find(".active").attr("href")
+        $(".fragment").each(function (index) {
+            let active_href = $(".character-main-nav")
+                .find(".active")
+                .attr("href");
 
-            $(this).load($(this).data('fragment-url'), function (response, status, xhr) {
-                $(this).children(':first').unwrap() // keep the original fragment container
-                $('[data-bs-toggle="popover"]').popover()
-                if (active_href !== undefined) {  // set the prior active tab in the character nav
-                    $('.character-main-nav a[href="' + active_href + '"]').tab('show')
-                }
-            })
-        })
+            $(this).load(
+                $(this).data("fragment-url"),
+                function (response, status, xhr) {
+                    $(this).children(":first").unwrap(); // keep the original fragment container
+                    $('[data-bs-toggle="popover"]').popover();
+                    if (active_href !== undefined) {
+                        // set the prior active tab in the character nav
+                        $(
+                            '.character-main-nav a[href="' + active_href + '"]',
+                        ).tab("show");
+                    }
+                },
+            );
+        });
 
-        let sc = $('.sidebar-content')
-        if (sc.data('sidebar-url') !== undefined) {
-            sc.load(sc.data('sidebar-url'), (response, status, xhr) => {
+        let sc = $(".sidebar-content");
+        if (sc.data("sidebar-url") !== undefined) {
+            sc.load(sc.data("sidebar-url"), (response, status, xhr) => {
                 if (xhr.status === 404) {
-                    $('#sidebar-right').css('width', '');
+                    $("#sidebar-right").css("width", "");
                 }
-            })
+            });
         }
     }
 
-    if (timeout) clearTimeout(timeout)
-    timeout = setTimeout(refresh, 500)
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(refresh, 500);
 }
