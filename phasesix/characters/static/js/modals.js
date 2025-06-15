aba4132d7145203c27e7c3b6105fa4138c2d3ef7
Sven <sven@nimbostratus.de>
2025-06-15 23:44:49 +0200

    Changed the contact and language edit widgets to be circles

--- phasesix/characters/static/js/modals.js
+++ phasesix/characters/static/js/modals.js
@@ -1,26 +1,24 @@
 $(function () {
-    let body = $('body')
-    let pageModal = $('#page-modal')
+    let body = $("body");
+    let pageModal = $("#page-modal");
 
-    body.on('click', '.modal-trigger', function (e) {
-        let modalDialog = $('#page-modal .modal-dialog')
-        $('.modal-header h5').text($(this).data('modal-title'))
-        pageModal.data('url', $(this).data('url'))
-        if ($(this).hasClass('small-modal')) {
-            modalDialog.removeClass('modal-xl')
+    body.on("click", ".modal-trigger", function (e) {
+        let modalDialog = $("#page-modal .modal-dialog");
+        $(".modal-title").text($(this).data("modal-title"));
+        pageModal.data("url", $(this).data("url"));
+        if ($(this).hasClass("small-modal")) {
+            modalDialog.removeClass("modal-xl");
         } else {
-            modalDialog.addClass('modal-xl')
+            modalDialog.addClass("modal-xl");
         }
-        $('#sidebar-right').css('width', '');
-    })
+        $("#sidebar-right").css("width", "");
+    });
 
-    pageModal.on('shown.bs.modal', function (e) {
-        $('.page-modal-container').load(
-            $(this).data('url')
-        )
-    })
+    pageModal.on("shown.bs.modal", function (e) {
+        $(".page-modal-container").load($(this).data("url"));
+    });
 
-    pageModal.on('hidden.bs.modal', function (e) {
-        $('.page-modal-container').html('')
-    })
-})
+    pageModal.on("hidden.bs.modal", function (e) {
+        $(".page-modal-container").html("");
+    });
+});
