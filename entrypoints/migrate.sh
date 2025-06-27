e32328ba57e2231e4bb01466528b087c1ad993a2
Sven <sven@nimbostratus.de>
2025-06-27 06:05:22 +0200

    Disabled clear_unused_images

--- entrypoints/migrate.sh
+++ entrypoints/migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /app/.venv/bin/python manage.py migrate
-/app/.venv/bin/python manage.py clear_unused_images
+# /app/.venv/bin/python manage.py clear_unused_images
 /app/.venv/bin/python manage.py collectstatic --noinput
 /app/.venv/bin/python manage.py compress -f
 
