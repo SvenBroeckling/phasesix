ff0ac54c436bcb09be23c7f68fcf9630dd04e911
Sven <sven@nimbostratus.de>
2025-08-11 10:46:00 +0200

    Reactivated the campaign join feature

--- phasesix/characters/static/js/campaign_detail.js
+++ phasesix/characters/static/js/campaign_detail.js
@@ -1,21 +1,20 @@
-$('.campaign-link').on('click', function (e) {
-    let elem = $(this)
-    let text = $(this).find(".invite-text")
-    let orig_text = text.text()
-    copyTextToClipboard(elem.attr('href'))
+$("body").on("click", ".campaign-link", function (e) {
+    let elem = $(this);
+    let text = $(this).find(".invite-text");
+    let orig_text = text.text();
+    copyTextToClipboard(elem.attr("href"));
 
-    text.text(elem.data('msg'))
-    setTimeout(() => text.text(orig_text), 2000)
+    text.text(elem.data("msg"));
+    setTimeout(() => text.text(orig_text), 2000);
 
-    e.preventDefault()
-    return false
-})
+    e.preventDefault();
+    return false;
+});
 
-$('table.campaign-status-sortable').tablesorter({
+$("table.campaign-status-sortable").tablesorter({
     textExtraction: {
-        '.data': function (node, table, cellIndex) {
-            return $(node).find('span.sort-key').text()
-        }
-    }
-})
-
+        ".data": function (node, table, cellIndex) {
+            return $(node).find("span.sort-key").text();
+        },
+    },
+});
