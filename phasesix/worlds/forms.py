7a5ae71fdacb7a897198c1732d01181dfb82c9ec
Sven <sven@nimbostratus.de>
2026-01-04 21:15:13 +0100

    Allow editing of the wiki page name

--- phasesix/worlds/forms.py
+++ phasesix/worlds/forms.py
@@ -18,10 +18,14 @@ class WikiPageTextForm(forms.ModelForm):
     class Meta:
         model = WikiPage
         fields = [
+            "name_de",
+            "name_en",
             "text_de",
             "text_en",
         ]
         widgets = {
+            "name_de": forms.TextInput(attrs={"class": "form-control"}),
+            "name_en": forms.TextInput(attrs={"class": "form-control"}),
             "text_de": forms.Textarea(attrs={"style": "height: 75vh"}),
             "text_en": forms.Textarea(attrs={"style": "height: 75vh"}),
         }
