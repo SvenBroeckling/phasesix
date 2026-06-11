0206b7f6aba5ea161e958b513bfa4aa4dd95740f
Sven <sven@nimbostratus.de>
2026-06-11 20:40:50 +0200

    Changed layout of essential characters to use a similar brand style

--- phasesix/essential_characters/templatetags/essential_extras.py
+++ phasesix/essential_characters/templatetags/essential_extras.py
@@ -23,3 +23,39 @@ def essential_attribute_rows(character):
 @register.simple_tag
 def essential_rank_options(maximum=4):
     return range(maximum + 1)
+
+
+@register.simple_tag
+def essential_condition_rows(character):
+    return [
+        {
+            "label": gettext("Wounds"),
+            "value": character.wounds,
+            "options": range(character.wound_threshold + 1),
+        },
+        {
+            "label": gettext("Burden"),
+            "value": character.burden,
+            "options": range(character.burden_threshold + 1),
+        },
+        {
+            "label": gettext("Omen"),
+            "value": character.omen,
+            "options": range(character.omen_max + 1),
+        },
+        {
+            "label": gettext("Arkana"),
+            "value": character.arkana,
+            "options": range(character.arkana_max + 1),
+        },
+        {
+            "label": gettext("Favor"),
+            "value": character.favor,
+            "options": range(character.favor_max + 1),
+        },
+        {
+            "label": gettext("Corruption"),
+            "value": character.corruption,
+            "options": range(max(6, character.corruption) + 1),
+        },
+    ]
